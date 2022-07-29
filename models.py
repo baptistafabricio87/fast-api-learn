457eba60c1c1886a03ece925c2131bba59726f7b
Fabricio Castro <baptistafabricio87@gmail.com>
2022-07-29 16:49:53 -0300

    Refactor and separete models & enums

---
+++ models.py
@@ -0,0 +1,11 @@
+"""Create Data Model"""
+
+from pydantic import BaseModel
+from typing import Union
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
